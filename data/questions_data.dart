f2a3b0e989a6c1aaa5ba8bc3b0d79a7c6c8ef1f7
ulsterrr <notthinginmyhand@gmail.com>
2022-12-14 18:53:25 +0700

    cấu trúc câu hỏi để import

---
+++ data/questions_data.dart
@@ -0,0 +1,41 @@
+const questionsData = [
+  {
+    'id': 1, // id của câu hỏi, dạng số nguyên
+      'catetoryId': 1, //id của lĩnh vực (thể loại), dạng số nguyên
+      'question': "Câu hỏi?", // câu hỏi, dạng string (text)
+      'quizAns1': "Câu trả lời 1", // câu trả lời, dạng string (text)
+      'quizAns2': "Câu trả lời 2", // câu trả lời, dạng string (text)
+      'quizAns3': "Câu trả lời 3", // câu trả lời, dạng string (text)
+      'quizAns4': "Câu trả lời 4", // câu trả lời, dạng string (text)
+      'answer': "Đáp án", // đáp án, dạng string (text)
+      'level': 1, // cấp độ khó, dạng số nguyên (tăng theo độ khó)
+      'manyTime': 60, // thời gian cho câu hỏi này (tuỳ theo cấp độ sẽ tăng thời gian), dạng số nguyên
+      'point': 100 // số điểm thưởng credit nếu trả lời đúng, dạng số nguyên
+  },
+  {
+    'id': 1, // id của câu hỏi, dạng số nguyên
+      'catetoryId': 1, //id của lĩnh vực (thể loại), dạng số nguyên
+      'question': "Câu hỏi?", // câu hỏi, dạng string (text)
+      'quizAns1': "Câu trả lời 1", // câu trả lời, dạng string (text)
+      'quizAns2': "Câu trả lời 2", // câu trả lời, dạng string (text)
+      'quizAns3': "Câu trả lời 3", // câu trả lời, dạng string (text)
+      'quizAns4': "Câu trả lời 4", // câu trả lời, dạng string (text)
+      'answer': "Đáp án", // đáp án, dạng string (text)
+      'level': 1, // cấp độ khó, dạng số nguyên (tăng theo độ khó)
+      'manyTime': 60, // thời gian cho câu hỏi này (tuỳ theo cấp độ sẽ tăng thời gian), dạng số nguyên
+      'point': 100 // số điểm thưởng credit nếu trả lời đúng, dạng số nguyên
+  },
+  {
+    'id': 1, // id của câu hỏi, dạng số nguyên
+      'catetoryId': 1, //id của lĩnh vực (thể loại), dạng số nguyên
+      'question': "Câu hỏi?", // câu hỏi, dạng string (text)
+      'quizAns1': "Câu trả lời 1", // câu trả lời, dạng string (text)
+      'quizAns2': "Câu trả lời 2", // câu trả lời, dạng string (text)
+      'quizAns3': "Câu trả lời 3", // câu trả lời, dạng string (text)
+      'quizAns4': "Câu trả lời 4", // câu trả lời, dạng string (text)
+      'answer': "Đáp án", // đáp án, dạng string (text)
+      'level': 1, // cấp độ khó, dạng số nguyên (tăng theo độ khó)
+      'manyTime': 60, // thời gian cho câu hỏi này (tuỳ theo cấp độ sẽ tăng thời gian), dạng số nguyên
+      'point': 100 // số điểm thưởng credit nếu trả lời đúng, dạng số nguyên
+  },
+];
