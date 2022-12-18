87310b51c0fc083dbf1b7c6f0ef56629ab450935
ulsterrr <notthinginmyhand@gmail.com>
2022-12-18 16:14:12 +0700

    update xử lý load câu hỏi và lĩnh vực

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\HocTap\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\doan\Who_Wants_To_Be_A_Millionaire"
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\StudyO\Flutter\Who_Wants_To_Be_A_Millionaire"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
